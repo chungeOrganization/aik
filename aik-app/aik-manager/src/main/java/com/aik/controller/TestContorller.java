be726efc228cf92bf387930f6524ca5327f0e4ea
as <88452486@qq.com>
2017-09-22 01:04:55 +0800

    【1.0.0-manager】 后台架构提交

---
+++ aik-app/aik-manager/src/main/java/com/aik/controller/TestContorller.java
@@ -0,0 +1,8 @@
+package com.aik.controller;
+
+/**
+ * Description:
+ * Created by as on 2017/9/22.
+ */
+public class TestContorller {
+}
---
+++ aik-app/aik-manager/src/main/java/com/aik/service/TestService.java
@@ -0,0 +1,8 @@
+package com.aik.service;
+
+/**
+ * Description:
+ * Created by as on 2017/9/22.
+ */
+public class TestService {
+}
