79d2d9bbd509f4aeeb3356d3e837df448ba086b9
daixiangning <1076143369@qq.com>
2017-10-19 20:45:38 +0800

    不提交

---
+++ aik-app/aik-dao/src/main/java/com/aik/vo/AccDoctorAccountVo.java
@@ -0,0 +1,258 @@
+package com.aik.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class AccDoctorAccountVo {
+    private Integer id;
+
+    private String mobileNo;
+
+    private String userName;
+
+    private String password;
+
+    private String realName;
+
+    private Byte sex;
+
+    private String areaProvince;
+
+    private String areaCity;
+
+    private Date birthday;
+
+    private String identityCard;
+
+    private String email;
+
+    private String hosName;
+
+    private String hosDepartment;
+
+    private String skill;
+
+    private String departmentPhone;
+
+    private String devType;
+
+    private String headImg;
+
+    private Byte position;
+
+    private BigDecimal price;
+
+    private BigDecimal starLevel;
+
+    private Date createDate;
+
+    private Date updateDate;
+    
+    private Byte deleteStatus;
+    
+    private String starttime;
+    
+    private String endtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo == null ? null : mobileNo.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName == null ? null : realName.trim();
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+
+    public String getAreaProvince() {
+        return areaProvince;
+    }
+
+    public void setAreaProvince(String areaProvince) {
+        this.areaProvince = areaProvince == null ? null : areaProvince.trim();
+    }
+
+    public String getAreaCity() {
+        return areaCity;
+    }
+
+    public void setAreaCity(String areaCity) {
+        this.areaCity = areaCity == null ? null : areaCity.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getIdentityCard() {
+        return identityCard;
+    }
+
+    public void setIdentityCard(String identityCard) {
+        this.identityCard = identityCard == null ? null : identityCard.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getHosName() {
+        return hosName;
+    }
+
+    public void setHosName(String hosName) {
+        this.hosName = hosName == null ? null : hosName.trim();
+    }
+
+    public String getHosDepartment() {
+        return hosDepartment;
+    }
+
+    public void setHosDepartment(String hosDepartment) {
+        this.hosDepartment = hosDepartment == null ? null : hosDepartment.trim();
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill == null ? null : skill.trim();
+    }
+
+    public String getDepartmentPhone() {
+        return departmentPhone;
+    }
+
+    public void setDepartmentPhone(String departmentPhone) {
+        this.departmentPhone = departmentPhone == null ? null : departmentPhone.trim();
+    }
+
+    public String getDevType() {
+        return devType;
+    }
+
+    public void setDevType(String devType) {
+        this.devType = devType == null ? null : devType.trim();
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg == null ? null : headImg.trim();
+    }
+
+    public Byte getPosition() {
+        return position;
+    }
+
+    public void setPosition(Byte position) {
+        this.position = position;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getStarLevel() {
+        return starLevel;
+    }
+
+    public void setStarLevel(BigDecimal starLevel) {
+        this.starLevel = starLevel;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+	public Byte getDeleteStatus() {
+		return deleteStatus;
+	}
+
+	public void setDeleteStatus(Byte deleteStatus) {
+		this.deleteStatus = deleteStatus;
+	}
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+
+	public String getEndtime() {
+		return endtime;
+	}
+
+	public void setEndtime(String endtime) {
+		this.endtime = endtime;
+	}
+	
+    
+}
