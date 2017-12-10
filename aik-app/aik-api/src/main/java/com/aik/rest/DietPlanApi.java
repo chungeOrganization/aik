708a3c2d65e44cd888d04d5df7ca4a23ebc8889e
as <88452486@qq.com>
2017-12-10 10:33:25 +0800

    【1.0.0 API】 用户端

--- aik-app/aik-api/src/main/java/com/aik/rest/DietPlanApi.java
+++ aik-app/aik-api/src/main/java/com/aik/rest/DietPlanApi.java
@@ -22,7 +22,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.*;
 
