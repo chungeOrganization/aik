60bef87e8acb5773e169f2f8ec127c97f4d94c29
as <88452486@qq.com>
2018-01-24 08:14:16 +0800

    修复

---
+++ aik-app/aik-manager/src/main/java/com/aik/interceptor/CommonInterceptor.java
@@ -0,0 +1,52 @@
+package com.aik.interceptor;
+
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.aik.model.SysUser;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by NERV on 2016/12/16.
+ */
+public class CommonInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        
+    	//返回上下文
+    	String path = httpServletRequest.getContextPath();
+        String scheme = httpServletRequest.getScheme();
+        String serverName = httpServletRequest.getServerName();
+        int port = httpServletRequest.getServerPort();
+        String basePath = scheme + "://" + serverName + ":" + port + path;
+        httpServletRequest.setAttribute("basePath", basePath);
+        
+        //如果是登陆 不拦截
+        String url = httpServletRequest.getServletPath();
+        if("/".equals(url) || "".equals(url) || "/login".equals(url) || "/logout".equals(url)){
+        	return true;
+        }
+
+        //登陆拦截  没有登陆跳转到登陆界面
+        Object obj = httpServletRequest.getSession().getAttribute("user");
+        if (obj == null || !(obj instanceof SysUser)) {
+        	httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/login");
+            return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
---
+++ aik-app/aik-manager/src/main/java/com/aik/interceptor/CommonInterceptorConfig.java
@@ -0,0 +1,16 @@
+package com.aik.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by NERV on 2016/12/16.
+ */
+@Configuration
+public class CommonInterceptorConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CommonInterceptor()).addPathPatterns("/**");
+    }
+}
